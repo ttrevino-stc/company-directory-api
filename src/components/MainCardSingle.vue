8abcc5afbe77964ec194c2a0e018bfa9f519440d
Thomas Trevino <ttrevino@stu.southtexascollege.edu>
2025-04-20 17:16:17 -0500

    connect to api is working

--- src/components/MainCardSingle.vue
+++ src/components/MainCardSingle.vue
@@ -1,20 +1,40 @@
 <script setup>
-    import {faker} from '@faker-js/faker'
-    const firstname = faker.person.firstName()
-    const lastname = faker.person.lastName()
-    const jobtitle = faker.person.jobTitle()
-    const bio = faker.lorem.lines(2)
-    const username = faker.internet.username({firstName: firstname, lastName: lastname}).toLowerCase()
+import { faker } from '@faker-js/faker'
+    
+// const firstname = faker.person.firstName()
+// const lastname = faker.person.lastName()
+// const jobtitle = faker.person.jobTitle()
+// const bio = faker.lorem.lines(2)
+// const username = faker.internet.username({ firstName: firstname, lastName: lastname }).toLowerCase()
+
+const props = defineProps({
+    employee: {
+        type: Object,
+        required: true,
+        default: () => {
+            return {
+                _id: "67fc7560b01e63d1d1399d5d",
+                userName: "jperez",
+                firstName: "Joseph",
+                lastName: "Perez",
+                title: "Computer Science Wizard",
+                quote: "You shall not pass!!",
+                __v: 0,
+            }
+        },
+    },
+})
+    
 </script>
 
 <template>
-    <RouterLink :to="{name: 'CardDetail', params: {id: username}}">
+    <RouterLink :to="{name: 'CardDetail', params: {id: props.employee._id}}">
         <div class="border border-gray-500 rounded-lg shadow bg-gray-400 h-full">
             <img class="object-fill h-480 w-960 rounded-t-lg" v-bind:src="faker.image.urlLoremFlickr({category: 'cats', height: 480, width: 960})" />
             <div class="p-4">
-                <p class="mb-2 text-2xl font-bold">{{ firstname }} {{ lastname }}</p>
-                <p class="mb-3 font-normal text-gray-800">{{ jobtitle }}</p>
-                <p class="mb-3 font-normal text-gray-800">{{ bio }}</p>
+                <p class="mb-2 text-2xl font-bold">{{ props.employee.firstName }} {{ props.employee.lastName }}</p>
+                <p class="mb-3 font-normal text-gray-800">{{ props.employee.title }}</p>
+                <p class="mb-3 font-normal text-gray-800">{{ props.employee.quote }}</p>
             </div>
 
         </div>
--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,10 +1,23 @@
 <script setup>
-import { ref } from 'vue'
+import { onMounted } from 'vue'
 import MainCards from '@/components/MainCards.vue'
 import MainPagination from '@/components/MainPagination.vue';
+import useAPI from '@/composables/useAPI';
+
+const { getEmployees, loading } = useAPI()
+
+onMounted(async () => {
+  await getEmployees()
+})
+
 </script>
 
 <template>
+
+<p v-if="loading">LOADING...</p>
+<div v-else>
   <MainCards></MainCards>
   <MainPagination></MainPagination>
+</div>
+  
 </template>
